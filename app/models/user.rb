34a201eb1a82edcb18a513825fe577f0692a7456
Bastekkkk <bastian.jarosz@gmail.com>
2018-10-03 13:26:25 +0200

    add user to articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+	has_many :articles
+	before_save {self.email = email.downcase}
+  validates :name, presence: true, length: { minimum: 5, maximum: 10 }, uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { minimum: 5, maximum: 10 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
 end
