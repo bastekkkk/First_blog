214bfc5cdb13059dac2408367da2e38e70d1ef1f
Bastekkkk <bastian.jarosz@gmail.com>
2018-10-04 12:52:33 +0200

    Add edit and update user profil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,22 @@ end
   		@users = User.all
     end
 
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:success] = "Successfuly updated"
+        redirect_to articles_path
+      else
+        render 'edit'
+    end
+  end
+  
+
+
   private
 
   def user_params
