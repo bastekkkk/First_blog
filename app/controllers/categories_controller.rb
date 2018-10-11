abbf2c14e803b846eaff64246db9129f24d1affc
Bastekkkk <bastian.jarosz@gmail.com>
2018-10-11 16:16:39 +0200

    Add test integration and created index and new html

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,32 @@
 class CategoriesController < ApplicationController
+
   def index
+  	@categories = Category.all
   end
 
   def home
   end
 
   def new
+  	@category = Category.new
   end
+
+  def create
+  	@category = Category.new(category_params)
+  	if @category.save
+  		flash[:success] = "successfuly created category"
+  		redirect_to categories_path
+  	else
+  		render 'new'
+  	end
+
+  end
+
+private
+
+def category_params
+	params.require(:category).permit(:name)
+end
+
+
 end
